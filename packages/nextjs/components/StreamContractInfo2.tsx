fb7d326fc6d4fe4b6d0f5fb5132d62690618d9b5
Carlos Sánchez <oceanrdn@gmail.com>
2023-12-06 13:00:36 +0100

    Temp pages

---
+++ packages/nextjs/components/StreamContractInfo2.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { BigNumber } from "ethers";
+import { useAccount } from "wagmi";
+import { BanknotesIcon, QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+import { Address, Balance } from "~~/components/scaffold-eth";
+import { useDeployedContractInfo, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+
+export const StreamContractInfo2 = () => {
+  const { address } = useAccount();
+  const { data: streamContract } = useDeployedContractInfo("SandGardenStreams");
+
+  const { data: owner } = useScaffoldContractRead({
+    contractName: "SandGardenStreams",
+    functionName: "owner",
+  });
+
+  const { data: builderData } = useScaffoldContractRead({
+    contractName: "SandGardenStreams",
+    functionName: "streamedBuilders",
+    args: [address],
+  }) as {
+    data: { cap: BigNumber } | undefined;
+  };
+
+  const amIAStreamdBuilder = builderData?.cap.gt(0);
+
+  return (
+    <>
+      <div className="mt-16">
+        <p className="font-bold mb-2 text-secondary">
+          Stream Contract
+          <span
+            className="tooltip text-white font-normal"
+            data-tip="All streams and contributions are handled by a contract on Optimism"
+          >
+            <QuestionMarkCircleIcon className="h-5 w-5 inline-block ml-2" />
+          </span>
+        </p>
+        <div className="flex gap-2 items-baseline">
+          <div className="flex flex-col items-center">
+            <Address address={streamContract?.address} />
+            <span className="text-xs text-[#f01a37]">Optimism</span>
+          </div>{" "}
+          /
+          <Balance address={streamContract?.address} className="text-3xl" />
+        </div>
+        {address && amIAStreamdBuilder && (
+          <div className="mt-3">
+            <label
+              htmlFor="withdraw-modal"
+              className="btn btn-primary btn-sm px-2 rounded-full font-normal space-x-2 normal-case"
+            >
+              <BanknotesIcon className="h-4 w-4" />
+              <span>Withdraw</span>
+            </label>
+          </div>
+        )}
+      </div>
+      <div className="mt-8">
+        <p className="font-bold mb-2 text-secondary">Owner</p>
+        <Address address={owner} />
+      </div>
+    </>
+  );
+};
